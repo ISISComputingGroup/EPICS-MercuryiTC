0a054fa36931adc2f57feadbe9f7a72dd87ab038
Adrian Potter <Adrian.Potter@tessella.com>
2018-01-19 16:18:50 +0000

    Separate level and temp logic

---
+++ iocBoot/iocMercuryiTC/st-level.cmd
@@ -0,0 +1,13 @@
+## set LEVEL_NUM before calling
+## if LEVEL_NUM = 1 then the entries for LEVEL_NUM 1 in this IOC will be used for the setup
+
+stringiftest("LEVEL", "$(VI_LEVEL_$(LEVEL_NUM)=)")
+
+$(IFLEVEL) calc("PORT_NUM", "$(LEVEL_NUM)+4")
+$(IFLEVEL) lvDCOMConfigure("lvfp$(PORT_NUM)", "frontpanel_level", "${MERCURY_ITC}/data/lv_MercuryLevel.xml")
+
+## Load our record instances
+$(IFLEVEL) dbLoadRecords("db/MercuryLevel.db", "P=$(MYPVPREFIX)$(IOCNAME):LEVEL:$(LEVEL_NUM):,port=lvfp$(PORT_NUM)")
+
+$(IFLEVEL) epicsEnvSet(SIM$(PORT_NUM), $(MYPVPREFIX)$(IOCNAME):$(PORT_NUM):SIM)
+$(IFLEVEL) epicsEnvSet(DISABLE$(PORT_NUM), $(MYPVPREFIX)$(IOCNAME):$(PORT_NUM):DISABLE)
