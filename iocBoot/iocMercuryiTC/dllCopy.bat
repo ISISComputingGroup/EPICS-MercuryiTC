5ce21b32dcc197db90b2731f4307a299010be8b6
Tom Willemsen <tomwillemsen1995@gmail.com>
2019-12-15 16:24:36 +0000

    Add support for the pressure card (via LvDCOM)

---
+++ iocBoot/iocMercuryiTC/dllCopy.bat
@@ -0,0 +1,18 @@
+@ECHO OFF
+XCOPY /R /I /Q /Y C:\Instrument\Apps\EPICS\support\asyn\master\bin\windows-x64\*.dll %MYDIR%..\..\bin\windows-x64
+XCOPY /R /I /Q /Y C:\Instrument\Apps\EPICS\support\oncrpc\master\bin\windows-x64\*.dll %MYDIR%..\..\bin\windows-x64
+XCOPY /R /I /Q /Y C:\Instrument\Apps\EPICS\support\autosave\master\bin\windows-x64\*.dll %MYDIR%..\..\bin\windows-x64
+XCOPY /R /I /Q /Y C:\Instrument\Apps\EPICS\support\caPutLog\master\bin\windows-x64\*.dll %MYDIR%..\..\bin\windows-x64
+XCOPY /R /I /Q /Y C:\Instrument\Apps\EPICS\support\devIocStats\master\bin\windows-x64\*.dll %MYDIR%..\..\bin\windows-x64
+XCOPY /R /I /Q /Y C:\Instrument\Apps\EPICS\support\icpconfig\master\bin\windows-x64\*.dll %MYDIR%..\..\bin\windows-x64
+XCOPY /R /I /Q /Y C:\Instrument\Apps\EPICS\support\libjson\master\bin\windows-x64\*.dll %MYDIR%..\..\bin\windows-x64
+XCOPY /R /I /Q /Y C:\Instrument\Apps\EPICS\ISIS\lvDCOM\master\bin\windows-x64\*.dll %MYDIR%..\..\bin\windows-x64
+XCOPY /R /I /Q /Y C:\Instrument\Apps\EPICS\support\MySQL\master\bin\windows-x64\*.dll %MYDIR%..\..\bin\windows-x64
+XCOPY /R /I /Q /Y C:\Instrument\Apps\EPICS\support\pcre\master\bin\windows-x64\*.dll %MYDIR%..\..\bin\windows-x64
+XCOPY /R /I /Q /Y C:\Instrument\Apps\EPICS\support\pugixml\master\bin\windows-x64\*.dll %MYDIR%..\..\bin\windows-x64
+XCOPY /R /I /Q /Y C:\Instrument\Apps\EPICS\support\pvdump\master\bin\windows-x64\*.dll %MYDIR%..\..\bin\windows-x64
+XCOPY /R /I /Q /Y C:\Instrument\Apps\EPICS\support\seq\master\bin\windows-x64\*.dll %MYDIR%..\..\bin\windows-x64
+XCOPY /R /I /Q /Y C:\Instrument\Apps\EPICS\support\sqlite\master\bin\windows-x64\*.dll %MYDIR%..\..\bin\windows-x64
+XCOPY /R /I /Q /Y C:\Instrument\Apps\EPICS\support\utilities\master\bin\windows-x64\*.dll %MYDIR%..\..\bin\windows-x64
+XCOPY /R /I /Q /Y C:\Instrument\Apps\EPICS\support\zlib\master\bin\windows-x64\*.dll %MYDIR%..\..\bin\windows-x64
+XCOPY /R /I /Q /Y C:\Instrument\Apps\EPICS\base\master\bin\windows-x64\*.dll %MYDIR%..\..\bin\windows-x64
