ab518d0b19fafd63f27f36719a8a71d163954e87
Tom Willemsen <tomwillemsen1995@gmail.com>
2020-06-02 14:46:52 +0100

    Refactor

--- iocBoot/iocMercuryiTC/st-temp.cmd
+++ iocBoot/iocMercuryiTC/st-temp.cmd
@@ -1,10 +1,10 @@
 ## set TEMP_NUM before calling
 ## if TEMP_NUM = 1 then the entries for TEMP_NUM 1 in this IOC will be used for the setup
 
-stringiftest("TEMP", "$(VI_TEMP_$(TEMP_NUM)=)")
+stringiftest("TEMP", "$(TEMP_$(TEMP_NUM)=)")
 
 ## Load our record instances
-$(IFTEMP) dbLoadRecords("db/MercuryTemp.db", "P=$(MYPVPREFIX)$(IOCNAME):$(TEMP_NUM):,PORT=L0")
+$(IFTEMP) dbLoadRecords("db/MercuryTemp.db", "P=$(MYPVPREFIX)$(IOCNAME):$(TEMP_NUM):,PORT=L0,BOARD=$(TEMP_$(TEMP_NUM)=None)")
 
 $(IFTEMP) epicsEnvSet(SIM$(TEMP_NUM), $(MYPVPREFIX)$(IOCNAME):$(TEMP_NUM):SIM)
 $(IFTEMP) epicsEnvSet(DISABLE$(TEMP_NUM), $(MYPVPREFIX)$(IOCNAME):$(TEMP_NUM):DISABLE)
