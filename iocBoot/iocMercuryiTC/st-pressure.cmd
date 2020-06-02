d96bfa39161caccf952f85c814bebe45997759d8
Tom Willemsen <tomwillemsen1995@gmail.com>
2020-06-02 17:07:04 +0100

    add pressure cards

--- iocBoot/iocMercuryiTC/st-pressure.cmd
+++ iocBoot/iocMercuryiTC/st-pressure.cmd
@@ -1,13 +1,10 @@
 ## set PRESSURE_NUM before calling
 ## if PRESSURE_NUM = 1 then the entries for PRESSURE_NUM 1 in this IOC will be used for the setup
 
-stringiftest("PRESSURE", "$(VI_PRESSURE_$(PRESSURE_NUM)=)")
-
-$(IFPRESSURE) calc("PORT_NUM", "$(PRESSURE_NUM)+6")
-$(IFPRESSURE) lvDCOMConfigure("lvfp$(PORT_NUM)", "frontpanel_pressure", "${MERCURY_ITC}/data/lv_MercuryPressure.xml", "$(LVDCOM_HOST=)", $(LVDCOM_OPTIONS=6), "$(LVDCOM_PROGID=)", "$(LVDCOM_USER=)", "$(LVDCOM_PASS=)")
+stringiftest("PRESSURE", "$(PRESSURE_$(PRESSURE_NUM)=)")
 
 ## Load our record instances
-$(IFPRESSURE) dbLoadRecords("db/MercuryPressure.db", "P=$(MYPVPREFIX)$(IOCNAME):PRESSURE:$(PRESSURE_NUM):,port=lvfp$(PORT_NUM)")
+$(IFPRESSURE) dbLoadRecords("db/MercuryPressure.db", "P=$(MYPVPREFIX)$(IOCNAME):PRESSURE:$(PRESSURE_NUM):,PORT=L0,BOARD=$(PRESSURE_$(PRESSURE_NUM))")
 
 $(IFPRESSURE) epicsEnvSet(SIM$(PORT_NUM), $(MYPVPREFIX)$(IOCNAME):$(PORT_NUM):SIM)
 $(IFPRESSURE) epicsEnvSet(DISABLE$(PORT_NUM), $(MYPVPREFIX)$(IOCNAME):$(PORT_NUM):DISABLE)
--- iocBoot/iocMercuryiTC/st-temp.cmd
+++ iocBoot/iocMercuryiTC/st-temp.cmd
@@ -4,7 +4,7 @@
 stringiftest("TEMP", "$(TEMP_$(TEMP_NUM)=)")
 
 ## Load our record instances
-$(IFTEMP) dbLoadRecords("db/MercuryTemp.db", "P=$(MYPVPREFIX)$(IOCNAME):$(TEMP_NUM):,PORT=L0,BOARD=$(TEMP_$(TEMP_NUM)=None)")
+$(IFTEMP) dbLoadRecords("db/MercuryTemp.db", "P=$(MYPVPREFIX)$(IOCNAME):$(TEMP_NUM):,PORT=L0,BOARD=$(TEMP_$(TEMP_NUM))")
 
 $(IFTEMP) epicsEnvSet(SIM$(TEMP_NUM), $(MYPVPREFIX)$(IOCNAME):$(TEMP_NUM):SIM)
 $(IFTEMP) epicsEnvSet(DISABLE$(TEMP_NUM), $(MYPVPREFIX)$(IOCNAME):$(TEMP_NUM):DISABLE)
